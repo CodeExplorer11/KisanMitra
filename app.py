bcecf56165bc3805602f3979ac33940f895acf65
CodeExplorer11 <vidhibly05@gmail.com>
2026-04-09 09:54:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if not st.session_state.entered_app:
             left: 0;
             width: 100%;
             height: 100%;
-            background-image: url('https://cdn.pixabay.com/photo/2016/11/14/04/08/farmer-1822530_640.jpg');
+            background-image: url("https://seedballs.in/cdn/shop/articles/farmers_day.jpg?v%3D1766078534%26width%3D500&tbnid=RlsNKxP2FS9MqM&vet=1&imgrefurl=https://seedballs.in/blogs/blog/farmers-day-a-powerful-tribute-to-the-heroes-who-feed-the-world?srsltid%3DAfmBOopnwkDKXlplhKwnEADvnfam6_i_f2Sn9o2cIoE2-xl-FURpeHvw&docid=76GBYt0Qf-3NPM&w=500&h=750&hl=en-IN&source=sh/x/im/m1/4&kgs=075546ed03e1bd58&utm_source=sh/x/im/m1/4");
             background-size: cover;
             background-position: center;
             display: flex;
